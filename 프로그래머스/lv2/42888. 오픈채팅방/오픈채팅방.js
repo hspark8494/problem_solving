141b8de743ad130a2b3dd51fae79812f5372d6d3
Hyo-Seoung Park <88135001+hspark8494@users.noreply.github.com>
2022-09-21 20:27:49 +0900

    [level 2] Title: 오픈채팅방, Time: 112.95 ms, Memory: 93.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,11 @@
+function solution(record) {
+    const dict = {}
+    let answer = []
+    for(let s of record){
+        let token = s.split(" ");
+        if(token[2]) dict[token[1]] = token[2];
+        if(token[0] !== "Change") answer.push({act : token[0], id : token[1]})
+    }
+
+    return answer.map(e => dict[e.id] + '님이 ' + ((e.act==="Enter") ? '들어왔습니다.' : "나갔습니다."));
+}
