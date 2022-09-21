bb125bc8467639d59e5df47f286242cc93fb3a59
Hyo-Seoung Park <88135001+hspark8494@users.noreply.github.com>
2022-09-21 20:04:41 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.26 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,26 @@
+function solution(board, moves) {
+    const boardStack = [];
+    const resultStack = [];
+    let result = 0;
+    for (let i = 0; i < board[0].length; i++) {
+        let li = [];
+        for (let j = board[0].length - 1; j >= 0; j--) {
+            if (board[j][i] != 0) {
+                li.push(board[j][i])
+            }
+        }
+        boardStack.push(li);
+    }
+    moves.forEach(e => {
+        let r = boardStack[e - 1].pop();
+        if (r != undefined) {
+            if (resultStack[resultStack.length - 1] === r) {
+                resultStack.pop();
+                result = result + 2;
+            } else {
+                resultStack.push(r);
+            }
+        }
+    })
+    return result;
+}
