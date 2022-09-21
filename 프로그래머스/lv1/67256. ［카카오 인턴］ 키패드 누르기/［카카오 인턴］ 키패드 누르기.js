9ef15fc8d896d75bf8da91086508d44f6857b315
Hyo-Seoung Park <88135001+hspark8494@users.noreply.github.com>
2022-09-21 20:04:36 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.42 ms, Memory: 30.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -0,0 +1,39 @@
+function solution(numbers, hand) {
+    var answer = '';
+    const dist = [[3,1],[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
+    let right = [3, 2];
+    let left = [3, 0];
+    const getDist = (a, b) => Math.abs(a[0]-b[0]) + Math.abs(a[1]-b[1]);
+
+    for (let h of numbers) {
+        if ([1, 4, 7].indexOf(h) > -1) {
+            answer += "L";
+            left = dist[h];
+            continue;
+        } else if ([3, 6, 9].indexOf(h) > -1) {
+            answer += "R";
+            right = dist[h];
+            continue;
+        }
+        const rightDist = getDist(right,dist[h]);
+        const leftDist = getDist(left,dist[h]);
+
+        if(rightDist<leftDist){
+            right = dist[h];
+            answer += "R";
+        }else if(leftDist<rightDist){
+            left = dist[h];
+            answer += "L";
+        }else{
+            if(hand == "right"){
+                answer += "R";
+                right = dist[h];
+            }else{
+                answer += "L";
+                left = dist[h];
+            }
+        }
+    }
+
+    return answer;
+}
